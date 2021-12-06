90535569cfa694b4ef644f2d9c2c036b54beb731
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-07 04:49:41 +0900

    feat: define codepipeline resources

---
+++ terraform/codepipeline.tf
@@ -0,0 +1,348 @@
+variable "github_org" {
+  type = string
+}
+
+variable "github_repository" {
+  type    = string
+  default = "ecs-blue-green"
+}
+
+variable "github_branch" {
+  type    = string
+  default = "main"
+}
+
+resource "aws_codepipeline" "main" {
+  name     = var.project
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_artifact.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.main.arn
+        FullRepositoryId = "${var.github_org}/${var.github_repository}"
+        BranchName       = var.github_branch
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.main.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeployToECS"
+      input_artifacts = ["source_output", "build_output"]
+      version         = "1"
+
+      configuration = {
+        ApplicationName                = aws_codedeploy_app.main.name
+        DeploymentGroupName            = aws_codedeploy_deployment_group.main.deployment_group_name
+        TaskDefinitionTemplateArtifact = "source_output"
+        AppSpecTemplateArtifact        = "source_output"
+        Image1ArtifactName             = "build_output"
+        Image1ContainerName            = "IMAGE1_NAME"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "codepipeline_artifact" {
+  bucket = "${var.project}-codepipeline-artifact"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.project}-codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning",
+          "s3:PutObjectAcl",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.codepipeline_artifact.arn,
+          "${aws_s3_bucket.codepipeline_artifact.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "codestar-connections:UseConnection"
+        ]
+        Resource = aws_codestarconnections_connection.main.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "codebuild:BatchGetBuilds",
+          "codebuild:StartBuild"
+        ]
+        Resource : aws_codebuild_project.main.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "codedeploy:CreateDeployment",
+          "codedeploy:GetApplicationRevision",
+          "codedeploy:GetDeployment",
+          "codedeploy:GetDeploymentConfig",
+          "codedeploy:RegisterApplicationRevision"
+        ]
+        Resource = [
+          aws_codedeploy_app.main.arn,
+          aws_codedeploy_deployment_group.main.arn
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:ListTasks",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService"
+        ]
+        Resource = [
+          aws_ecs_cluster.main.arn,
+          aws_ecs_service.main.id,
+          "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:task-definition/${aws_ecs_task_definition.main.family}:*"
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "ima:PassRole"
+        Resource = data.aws_iam_role.ecsTaskExecutionRole.arn
+      }
+    ]
+  })
+}
+
+resource "aws_codestarconnections_connection" "main" {
+  name          = var.project
+  provider_type = "GitHub"
+}
+
+resource "aws_codebuild_project" "main" {
+  name         = var.project
+  description  = "codebuild_project for ${var.project}"
+  service_role = aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = aws_cloudwatch_log_group.codebuild.name
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild" {
+  name = "${var.project}-codebuild"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal : {
+          Service = "codebuild.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild" {
+  role = aws_iam_role.codebuild.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning",
+          "s3:PutObjectAcl",
+          "s3:PutObject"
+        ]
+        "Resource" = [
+          aws_s3_bucket.codepipeline_artifact.arn,
+          "${aws_s3_bucket.codepipeline_artifact.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "codebuild" {
+  name = "/${var.project}/codebuild"
+}
+
+resource "aws_codedeploy_app" "main" {
+  name             = var.project
+  compute_platform = "ECS"
+}
+
+resource "aws_codedeploy_deployment_group" "main" {
+  deployment_group_name  = var.project
+  app_name               = aws_codedeploy_app.main.name
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+  service_role_arn       = aws_iam_role.codedeploy.arn
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.main.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.main.arn]
+      }
+
+      test_traffic_route {
+        listener_arns = [aws_lb_listener.test.arn]
+      }
+
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name = "${var.project}-codedeploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal : {
+          Service = "codedeploy.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy" {
+  role       = aws_iam_role.codedeploy.name
+  policy_arn = data.aws_iam_policy.codedeploy_role.arn
+}
+
+data "aws_iam_policy" "codedeploy_role" {
+  name = "AWSCodeDeployRoleForECSLimited"
+}
